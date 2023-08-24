e3e313a4cc16ab9fb158db44176f142442cf5271
Yunjung <120556910+YunJ96@users.noreply.github.com>
2023-08-24 23:41:13 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
@@ -1,9 +1,3 @@
 function solution(slice, n) {
-    if (n % slice === 0) {
-        return n / slice;
-    } else if (slice > n) {
-        return 1;
-    } else if (n % slice != 0) {
-        return Math.floor(n / slice) + 1;
-    }
+    return Math.ceil(n / slice)
 }
