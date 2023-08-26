6456cf771a83ba15b11534557cd5aa3ee34209c1
Yunjung <120556910+YunJ96@users.noreply.github.com>
2023-08-26 10:23:14 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -1,7 +1,3 @@
 function solution(money) {
-    if (money < 5500) return [0, money];
-    let answer = [];
-    answer.push(Math.floor(money / 5500));
-    answer.push(money - (Math.floor(money / 5500) * 5500));
-    return answer;
+    return [Math.floor(money / 5500), money % 5500];
 }
