4f3c68a3fb7b6060dca7e7bc2f93e6ea098b85a8
Yunjung <120556910+YunJ96@users.noreply.github.com>
2023-09-19 14:08:19 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -0,0 +1,13 @@
+function solution(my_string) {
+    const arr = my_string.split(' ');
+    let answer = parseInt(arr[0]);
+    
+    for (let i = 1; i < arr.length; i += 2) {
+        if (arr[i] === '+') {
+            answer += parseInt(arr[i + 1]);
+        } else {
+            answer -= parseInt(arr[i + 1]);
+        }
+    }
+    return answer;
+}
